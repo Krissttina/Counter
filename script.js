1fb040fb608377e2190c79f7e7747eca6f9ba7dc
KristinaMetodieva <kristinametodieva621@gmail.com>
2023-04-25 19:03:44 +0300

    js update
    js code update

--- script.js
+++ script.js
@@ -1,44 +1,17 @@
-// buttons and counter text
-const counter = document.querySelector("#counter");
-const btns = document.querySelectorAll(".btn");
-
-// initialize the count variable
+let number = document.getElementById('counter');
 let count = 0;
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const styles = e.currentTarget.classList;
+document.querySelector('.resetBtn').addEventListener('click', () => {
+  count = 0;
+  number.innerHTML = count;
+});
 
-    if (styles.contains("increase")) {
-      count++;
-      counter.classList.add("up");
+document.querySelector('.increaseBtn').addEventListener('click', () => {
+  count += 1;
+  number.innerHTML = count;
+});
 
-      setTimeout(() => {
-        counter.classList.remove("up");
-      }, 500);
-    } else if (styles.contains("decrease")) {
-      count--;
-      counter.classList.add("down");
-
-      setTimeout(() => {
-        counter.classList.remove("down");
-      }, 500);
-    } else {
-      count = 0;
-    }
-
-    if (count > 0) {
-      counter.style.color = "green";
-    }
-
-    if (count < 0) {
-      counter.style.color = "red";
-    }
-
-    if (count === 0) {
-      counter.style.color = "grey";
-    }
-
-    counter.textContent = count;
-  });
+document.querySelector('.decreaseBtn').addEventListener('click', () => {
+  count -= 1;
+  number.innerHTML = count;
 });
